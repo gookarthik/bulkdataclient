35903e43e0ad159c5f5e1216e4e2f6a16f5b6462
Karthik Sharma <karthikmn0821@gmail.com>
2018-09-11 16:10:47 +0530

    First Commit

---
+++ src/app/config/config-stepper/config-stepper.component.ts
@@ -0,0 +1,54 @@
+import { NdJsonDialogComponent } from './../../extract/ndjson-dialog/ndjson-dialog.component';
+
+import { Component, OnInit, Input } from '@angular/core';
+import { ConfigService } from './../config.service';
+import { MatDialog } from '@angular/material';
+
+@Component({
+  selector: 'app-config-stepper',
+  templateUrl: './config-stepper.component.html',
+  styleUrls: ['./config-stepper.component.scss']
+})
+export class ConfigStepperComponent implements OnInit {
+  @Input() data;
+  getApiData: any = {};
+  getPollData: any = {};
+  getDownloadData: any = {};
+  downloadContent: any = [];
+  constructor(private configService: ConfigService, private dialog: MatDialog) {
+    // if ((this.configService.isLoadingResults = true)) {
+
+    // }
+
+    setTimeout(function() {
+      const elmnt = document.getElementById('scroll');
+      console.log(elmnt);
+      elmnt.scrollIntoView();
+    }, 300);
+  }
+
+  ngOnInit() {
+    console.log(this.data);
+    this.getApiData = this.data.getApiData;
+    this.getPollData = this.data.getPollData;
+    this.getDownloadData = this.data.getDownloadData;
+    this.downloadContent = this.getDownloadData;
+  }
+
+  getSelectedNdJson(resourceName, resourceLink) {
+    this.configService
+      .getSelectedNdJson(resourceLink)
+      .subscribe((res: Response) => {
+        const config = {
+          minWidth: 800,
+          hasBackdrop: true,
+          data: {
+            ndjson: res.body,
+            link: resourceLink,
+            name: resourceName
+          }
+        };
+        const dialogRef = this.dialog.open(NdJsonDialogComponent, config);
+      });
+  }
+}
